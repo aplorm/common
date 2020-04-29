f7c6f1252245c45ab1fe61caed0e94d341bb9b17
nicolas moral <nicolas.moral@acseo-conseil.fr>
2020-04-29 19:51:51 +0200

    add float and number has type instead of global number

--- src/Interpreter/TypeInterface.php
+++ src/Interpreter/TypeInterface.php
@@ -17,8 +17,9 @@ interface TypeInterface
     public const STRING_TYPE = 1;
     public const CLASS_CONSTANT_TYPE = 2;
     public const OTHER_CONSTANT_TYPE = 3;
-    public const NUMBER_CONSTANT_TYPE = 4;
-    public const ARRAY_TYPE = 5;
-    public const OBJECT_TYPE = 6;
-    public const ANNOTATION_TYPE = 7;
+    public const INT_CONSTANT_TYPE = 8;
+    public const FLOAT_CONSTANT_TYPE = 7;
+    public const ARRAY_TYPE = 4;
+    public const OBJECT_TYPE = 5;
+    public const ANNOTATION_TYPE = 6;
 }
